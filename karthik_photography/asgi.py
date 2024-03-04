0bc803fd11160ef0a2d9e232ae91fefcd9f4a9f0
sivabala21 <sivabalahack@gmail.com>
2024-03-04 14:41:51 +0530

    first commit

---
+++ karthik_photography/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for karthik_photography project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'karthik_photography.settings')
+
+application = get_asgi_application()
