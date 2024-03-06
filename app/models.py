2fc5a9482028c111fce55c753214943c84c896fe
sivabala21 <sivabalahack@gmail.com>
2024-03-06 14:44:38 +0530

    Book init

--- app/models.py
+++ app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class form(models.Model):
+    name = models.CharField( max_length=50),
+    email=models.EmailField( max_length=254),
+    
+    appointment_date=models.DateField(auto_now=False, auto_now_add=False),
--- app/views.py
+++ app/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from django import forms
 
 # Create your views here.
+
+#forms
+
+class AppointmentForm(forms.Form):
+    name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'}))
+    email = forms.EmailField(max_length=254, widget=forms.EmailInput(attrs={'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'}))
+    phoneno = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'}))
+    appointment_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date', 'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'}))
+
+
 
 def home(request):
 
@@ -15,7 +26,8 @@ def portraits(request):
 
 def book(request):
     
-    return render(request,"book.html")
+    form=AppointmentForm()
+    return render(request,"book.html",{'form': form})
 
 
 def about(request):
